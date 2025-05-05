f987dec6117bb9b02d1df415fd5539313b42a7dd
Taylor Timmothy Johnson <becker@M2-Air.local>
2025-05-05 15:58:28 -0400

    more caching fix4

--- spec/spec_helper_e2e.rb
+++ spec/spec_helper_e2e.rb
@@ -10,8 +10,21 @@ RSpec.configure do |c|
     # Try to load cached container first
     cached = load_container_state
 
-    # Only run setup if we didn't restore from cache
-    unless cached
+    if cached
+      puts "Restored from cached container, running minimal setup..."
+      # Install Apache first since it's not in the container
+      if apt?
+        install_packages(['apache2'])
+      else
+        install_packages(['httpd'])
+      end
+      # Then continue with service setup
+      fix_apache
+      upload_portal_config('portal.yml')
+      update_ood_portal
+      restart_apache
+      restart_dex
+    else
       puts "No cached container found, running full setup..."
       bootstrap_repos
       ondemand_repo
@@ -23,11 +36,6 @@ RSpec.configure do |c|
       restart_dex
       bootstrap_user
       bootstrap_flask
-    else
-      puts "Restored from cached container, skipping setup..."
-      # Maybe still need to restart services
-      restart_apache
-      restart_dex
     end
   end
 
