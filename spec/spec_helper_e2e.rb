fe0c5b518659109d2eb8091e3b5d011e70b32217
becker63 <johnsontaylor6320@gmail.com>
2025-05-12 17:56:42 -0400

    more debug info

--- spec/spec_helper_e2e.rb
+++ spec/spec_helper_e2e.rb
@@ -2,6 +2,7 @@
 
 require 'beaker-rspec'
 require 'e2e/e2e_helper'
+require 'net/http'
 
 RSpec.configure do |c|
   # Readable test descriptions
@@ -15,6 +16,28 @@ RSpec.configure do |c|
     bootstrap_user
     # Needed by node/rnode proxy tests
     bootstrap_flask
+
+    # Add wait loop for Dex to be ready
+    uri = URI('http://localhost:5556/.well-known/openid-configuration')
+    Timeout.timeout(30) do
+      loop do
+        begin
+          resp = Net::HTTP.get_response(uri)
+          break if resp&.code == '200'
+        rescue StandardError
+          # service not up yet
+        end
+        sleep 1
+      end
+    end
+    
+    # Add debug output
+    on hosts, 'systemctl status ondemand-dex' do
+      puts "Dex status: #{stdout}"
+    end
+    on hosts, 'curl -v http://localhost:5556/.well-known/openid-configuration' do
+      puts "Dex response: #{stdout}"
+    end
   end
 
   c.after(:suite) do
