186849232ed8d3d670afe95c96f444355e3e20d1
becker63 <johnsontaylor6320@gmail.com>
2025-05-12 16:36:12 -0400

    hope this works

--- spec/spec_helper_e2e.rb
+++ spec/spec_helper_e2e.rb
@@ -7,16 +7,13 @@ RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
   c.before(:suite) do
-    bootstrap_repos
-    ondemand_repo
-    install_ondemand
     fix_apache
     upload_portal_config('portal.yml')
     update_ood_portal
     restart_apache
     restart_dex
     bootstrap_user
-    # Need by node/rnode proxy tests
+    # Needed by node/rnode proxy tests
     bootstrap_flask
   end
 
